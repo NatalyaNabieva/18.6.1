71cf340c8cb679f4239ea4e321fe6cdb35c9c361
Наталья Набиева <101559839+NatalyaNabieva@users.noreply.github.com>
2022-08-03 10:12:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 import telebot
 from config import keys, TOKEN
-from extensions import ConvertionException, CryptoConverter
+from extensions import APIException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
     text = f'Здравствуйте, {message.from_user.first_name}! Я бот-конвертер валют. \
-Чтобы воспользоваться мной, введите данные через пробел в следующем формате: \
-\nназвание валюты, \
+Чтобы начать работу, введите данные через пробел в следующем формате: \
+"Название валюты, \
 в какую валюту хотите перевести, \
-количество валюты. \
+количество валюты". \
 \nЧтобы увидеть список доступных валют, введите команду: /values'
     bot.reply_to(message, text)
 
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
-    text = 'Доступные валюты:'
+    text = 'Доступные валюты: '
     for key in keys.keys():
-        text = '\n'.join((text, key, ))
+        text = '\n'.join((text, key,))
     bot.reply_to(message, text)
 
 @bot.message_handler(content_types=['text', ])
@@ -27,16 +27,19 @@ def convert(message: telebot.types.Message):
         values = message.text.split(' ')
 
         if len(values) != 3:
-            raise ConvertionException('Слишком много параметров.')
-
+            raise APIException('Неверное количество параментров. Введите команду боту в следующем вормате:\
+    <имя валюты>\
+    <в какую валюту перевести> \
+    <количество переводимой валюты>\nУвидеть список всех доступных валют: /values')
         quote, base, amount = values
-        total_base = CryptoConverter.convert(quote, base, amount)
-    except ConvertionException as e:
+        total_base = CryptoConverter.get_price(quote, base, amount)
+    except APIException as e:
         bot.reply_to(message, f'Ошибка пользователя.\n{e}')
     except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+        bot.reply_to(message, f'Не удалось обработать команду.\n{e} ')
     else:
-        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        p = total_base * float(amount)
+        text = f'Цена {amount} {quote} в {base} - {p}'
         bot.send_message(message.chat.id, text)
 
 bot.polling(none_stop=True)
